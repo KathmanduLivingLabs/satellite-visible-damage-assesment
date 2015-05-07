779e170bd41b24a4aef6b73e6285f1283de0720f
unknown <gautamxpratik@gmail.com>
2015-05-07 11:54:46 +0545

    boilerplate;

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var map = L.map("map", {
+        center: [27.8006, 85.3934],
+        zoom: 8,
+        layers: [L.tileLayer("https://{s}.tiles.mapbox.com/v4/kll.m410hgod/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA")]
+    });
+});
